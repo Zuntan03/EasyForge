0b9ac2d72de0056964c1f798cc6c356c77950657
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-08-16 03:14:49 +0900

    Flux1 Schnell GGUF 追加

--- download/Flux-Gguf.bat
+++ download/Flux-Gguf.bat
@@ -2,8 +2,11 @@
 chcp 65001 > NUL
 
 call %~dp0Stable-diffusion\Flux\flux1-dev-Q4_0.bat
-call %~dp0Stable-diffusion\Flux\flux1-dev-Q5_0.bat
+call %~dp0Stable-diffusion\Flux\flux1-dev-Q5_1.bat
 call %~dp0Stable-diffusion\Flux\flux1-dev-Q8_0.bat
+call %~dp0Stable-diffusion\Flux\flux1-schnell-Q4_0.bat
+call %~dp0Stable-diffusion\Flux\flux1-schnell-Q5_1.bat
+call %~dp0Stable-diffusion\Flux\flux1-schnell-Q8_0.bat
 
 call %~dp0VAE\Flux\ae.bat
 call %~dp0VAE\Flux\clip_l.bat
--- download/Stable-diffusion/Flux/flux1-dev-Q5_1.bat
+++ download/Stable-diffusion/Flux/flux1-dev-Q5_1.bat
@@ -4,7 +4,7 @@ set DL_HF=%~dp0..\..\..\setup\download\HuggingFace.bat
 pushd %~dp0..\..\..\stable-diffusion-webui-forge\models\Stable-diffusion
 
 @REM https://huggingface.co/city96/FLUX.1-dev-gguf
-call %DL_HF% Flux flux1-dev-Q5_0.gguf city96/FLUX.1-dev-gguf
+call %DL_HF% Flux flux1-dev-Q5_1.gguf city96/FLUX.1-dev-gguf
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\stable-diffusion-webui-forge\models\Stable-diffusion
