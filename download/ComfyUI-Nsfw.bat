5c244f4c1d6ce73f9fe524846a4c21c5ed98f31d
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-09-15 07:59:49 +0900

    LLM調整

--- download/ComfyUI-Nsfw.bat
+++ download/ComfyUI-Nsfw.bat
@@ -3,7 +3,7 @@ chcp 65001 > NUL
 
 call %~dp0ComfyUI-Minimum.bat
 
-call %~dp0ComfyUI\llm_gguf\Vecteus-v1-Q5_K_M.bat
+call %~dp0ComfyUI\llm_gguf\Vecteus-v1-Q4_K_M.bat
 
 call %~dp0Stable-diffusion\FluxD-Nsfw\iNiverseFluxV11-8step-Q4_K_S.bat
 call %~dp0Stable-diffusion\FluxD-Nsfw\iNiverseV11x05-FCAnimeV33x05-8step-Q4_K_S.bat
--- download/ComfyUI/llm_gguf/Vecteus-v1-Q4_K_M.bat
+++ download/ComfyUI/llm_gguf/Vecteus-v1-Q4_K_M.bat
@@ -4,7 +4,7 @@ set DL_HF=%~dp0..\..\..\setup\download\HuggingFace.bat
 pushd %~dp0..\..\..\ComfyUI\models
 
 @REM https://huggingface.co/mmnga/Vecteus-v1-gguf
-call %DL_HF% llm_gguf\ Vecteus-v1-Q5_K_M.gguf mmnga/Vecteus-v1-gguf
+call %DL_HF% llm_gguf\ Vecteus-v1-Q4_K_M.gguf mmnga/Vecteus-v1-gguf
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\..\ComfyUI\models
