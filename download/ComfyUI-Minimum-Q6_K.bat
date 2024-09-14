e73ac42326bf84875022b82d9ade216f4376efe7
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-09-14 20:39:12 +0900

    path fix

--- download/ComfyUI-Minimum-Q6_K.bat
+++ download/ComfyUI-Minimum-Q6_K.bat
@@ -3,8 +3,8 @@ chcp 65001 > NUL
 
 call %~dp0ComfyUI-Minimum.bat
 
-call %~dp0Stable-diffusion\FluxD-Anime\fca_style-v33-x07-8step-Q6_K.bat
-call %~dp0Stable-diffusion\FluxD-Anime\MythP0rt-x07-8step-Q6_K.bat
+call %~dp0Stable-diffusion\FluxD-Anime\fca_style-v33-x10-8step-Q6_K.bat
+call %~dp0Stable-diffusion\FluxD-Anime\MythP0rt-x08-8step-Q6_K.bat
 
 call %~dp0Stable-diffusion\FluxD-Nsfw\iNiverseFluxV11-8step-Q6_K.bat
 call %~dp0Stable-diffusion\FluxD-Nsfw\iNiverseV11x05-FCAnimeV33x05-8step-Q6_K.bat
--- download/ComfyUI-Minimum.bat
+++ download/ComfyUI-Minimum.bat
@@ -7,8 +7,8 @@ call %~dp0ComfyUI\llm_gguf\Llama-3.1-8B-EZO-1.1-it-Q4_K_M.bat
 
 call %~dp0Lora\FluxD-Util\Hyper-FLUX.1-dev-8steps-lora.bat
 
-call %~dp0Stable-diffusion\FluxD-Anime\fca_style-v33-x07-8step-Q4_K_S.bat
-call %~dp0Stable-diffusion\FluxD-Anime\MythP0rt-x07-8step-Q4_K_S.bat
+call %~dp0Stable-diffusion\FluxD-Anime\fca_style-v33-x10-8step-Q4_K_S.bat
+call %~dp0Stable-diffusion\FluxD-Anime\MythP0rt-x08-8step-Q4_K_S.bat
 
 call %~dp0Stable-diffusion\FluxD-Base\flux1-dev-hyper8-Q4_K_S.bat
 call %~dp0Stable-diffusion\FluxD-Base\flux1-dev-hyper8-Q6_K.bat
