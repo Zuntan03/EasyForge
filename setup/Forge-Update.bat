c8a6d3e4a2c9e71720e3feefa2a66dc646c95fee
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-09-21 22:26:25 +0900

    ImpactPackリリーストラブル仮対処

--- setup/Forge-Update.bat
+++ setup/Forge-Update.bat
@@ -20,3 +20,7 @@ if exist "%~dp0..\stable-diffusion-webui-forge\models\VAE\Flux\clip_l.safetensor
 		call "%~dp0..\download\VAE\Flux\t5xxl-Q6_K.bat"
 	)
 )
+
+if exist "%~dp0..\ComfyUI\" (
+	call "%~dp0ComfyUI-Update.bat"
+)
